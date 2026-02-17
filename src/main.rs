cacd718f62a4a027177b83e1105e741d5050bfd3
styxer <styxer@tutanota.com>
2026-02-17 21:43:55 +0100

    [LOG_VIEWER] Adding globbing

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use chrono::{DateTime, Utc};
+use chrono::DateTime;
 use clap::Parser;
-use rerun::SeriesLines;
+use glob::glob;
 use rerun::external::re_log;
+use rerun::Scalars;
 use std::fs;
 
 #[derive(Debug, clap::Parser)]
@@ -9,23 +10,35 @@ use std::fs;
 pub struct Args {
     /// Path to the trace file.
     #[arg(short, long)]
-    trace_file_path: String,
+    trace_file_directory: String,
 }
 
 fn main() -> anyhow::Result<()> {
     re_log::setup_logging();
     let args = Args::parse();
 
-    let contents = match fs::read_to_string(&args.trace_file_path) {
-        // If successful return the files text as `contents`.
-        // `c` is a local variable.
-        Ok(c) => c.split("\n").map(|s| s.to_owned()).collect::<Vec<String>>(),
-        // Handle the `error` case.
-        Err(_) => {
-            // Write `msg` to `stderr`.
-            panic!("Could not read file `{}`", args.trace_file_path);
+    let mut contents: Vec<String> = Vec::new();
+    for entry in glob(format!("{}/**/*.trace", &args.trace_file_directory).as_str())
+        .expect("Failed to read glob pattern")
+    {
+        match entry {
+            Ok(path) => {
+                match fs::read_to_string(&path) {
+                    // If successful return the files text as `contents`.
+                    // `c` is a local variable.
+                    Ok(c) => c.split("\n").map(|s| s.to_owned()).for_each(|e| {
+                        contents.push(e);
+                    }),
+                    // Handle the `error` case.
+                    Err(_) => {
+                        // Write `msg` to `stderr`.
+                        panic!("Could not read file `{}`", args.trace_file_directory);
+                    }
+                };
+            }
+            Err(e) => println!("{:?}", e),
         }
-    };
+    }
 
     let rec = rerun::RecordingStreamBuilder::new("OcppMeter values").spawn()?;
 
@@ -35,7 +48,6 @@ fn main() -> anyhow::Result<()> {
             .map(|s| s.to_owned())
             .collect::<Vec<_>>();
         if line_parts.len() != 10 {
-            println!("{}", line_parts.len());
             continue;
         }
 
@@ -49,7 +61,11 @@ fn main() -> anyhow::Result<()> {
         if date_time.is_empty() {
             continue;
         }
-        let timestamp = DateTime::parse_from_str(date_time.as_str(), "%Y-%m-%d %H:%M:%S %z")?;
+
+        let timestamp = match DateTime::parse_from_str(date_time.as_str(), "%Y-%m-%d %H:%M:%S %z") {
+            Ok(d) => d,
+            _ => continue,
+        };
 
         if let Ok(meter_vaules_request) = serde_json::from_str::<
             rust_ocpp::v1_6::messages::meter_values::MeterValuesRequest,
@@ -131,60 +147,59 @@ fn main() -> anyhow::Result<()> {
                 }
             }
 
+            rec.log_static(
+                format!("{}/current/import/L1", date),
+                &rerun::SeriesLines::new()
+                    .with_colors([[255, 0, 0]])
+                    .with_names(["Current.Import(L1)"])
+                    .with_widths([2.0]),
+            )?;
+
             rec.set_timestamp_secs_since_epoch("time", timestamp.timestamp() as f64);
             rec.log(
-                "current/import/L1",
-                &rerun::Scalars::single(current_import_l1.unwrap_or(0.0)),
+                format!("{}/current/import/L1", date),
+                &Scalars::single(current_import_l1.unwrap_or(0.0)),
             )?;
 
             rec.log(
                 "current/import/L2",
-                &rerun::Scalars::single(current_import_l2.unwrap_or(0.0)),
+                &Scalars::single(current_import_l2.unwrap_or(0.0)),
             )?;
 
             rec.log(
                 "current/offered",
-                &rerun::Scalars::single(current_offered.unwrap_or(0.0)),
+                &Scalars::single(current_offered.unwrap_or(0.0)),
             )?;
 
             rec.log(
                 "power/offered",
-                &rerun::Scalars::single(power_offered.unwrap_or(0.0)),
+                &Scalars::single(power_offered.unwrap_or(0.0)),
             )?;
 
-            rec.log(
-                "voltage/L1",
-                &rerun::Scalars::single(voltage_l1.unwrap_or(0.0)),
-            )?;
+            rec.log("voltage/L1", &Scalars::single(voltage_l1.unwrap_or(0.0)))?;
 
-            rec.log(
-                "voltage/L2",
-                &rerun::Scalars::single(voltage_l2.unwrap_or(0.0)),
-            )?;
+            rec.log("voltage/L2", &Scalars::single(voltage_l2.unwrap_or(0.0)))?;
 
-            rec.log(
-                "voltage/L3",
-                &rerun::Scalars::single(voltage_l3.unwrap_or(0.0)),
-            )?;
+            rec.log("voltage/L3", &Scalars::single(voltage_l3.unwrap_or(0.0)))?;
 
             rec.log(
                 "power/active/import/L1",
-                &rerun::Scalars::single(power_active_import_l1.unwrap_or(0.0)),
+                &Scalars::single(power_active_import_l1.unwrap_or(0.0)),
             )?;
 
             rec.log(
                 "power/active/import/L2",
-                &rerun::Scalars::single(power_active_import_l2.unwrap_or(0.0)),
+                &Scalars::single(power_active_import_l2.unwrap_or(0.0)),
             )?;
 
             rec.log(
                 "power/active/import/L3",
-                &rerun::Scalars::single(power_active_import_l3.unwrap_or(0.0)),
+                &Scalars::single(power_active_import_l3.unwrap_or(0.0)),
             )?;
 
             rec.log(
                 "power/active/import/sum",
-                &rerun::Scalars::single(
+                &Scalars::single(
                     power_active_import_l1.unwrap_or(0.0)
                         + power_active_import_l2.unwrap_or(0.0)
                         + power_active_import_l3.unwrap_or(0.0),
